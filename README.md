5c24ef00d11c7771f6e0a5a6c991140f91d4d524
Shianne <ShianneJT@gmail.com>
2020-05-17 19:04:16 -0600

    adds quantity function

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# bamazon
+# Bamazon
+
+(WIP) **Bamazon** Mock CLI storefront app.
+
+## Technology Used
+
+- JavaScript
+- MySQL Workbench
+- [Node.js](https://nodejs.org/en/)
+- [npm](https://www.npmjs.com) packages
+  - [mysql](https://www.npmjs.com/package/mysql)
+  - [Inquirer.js](https://www.npmjs.com/package/inquirer)
+  - [CLI Table](https://www.npmjs.com/package/cli-table)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,16 +1,11 @@
 var fs = require('fs');
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-// const cTable = require('console.table');
 var Table = require('cli-table');
- 
-// instantiate
 var itemTable = new Table({
     head: ['Item ID', 'Product', 'Department', 'Price'],
     colWidths: [10, 15, 15, 10]
 });
-
-
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -22,30 +17,63 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
     if (err) throw err;
-    logo();    
+    logo();
 });
 
 function logo() {
     fs.readFile('assets/logo.txt', 'utf8', function (err, data) {
         if (err) {
             console.log(err);
-        }
-        else {
+        } else {
             console.log(data)
-            openStore();
+            displayItems();
         };
     });
 };
 
-function openStore(){
-    connection.query('SELECT * FROM products',
-    function (err, res) {
+function displayItems(){
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
 
         for (var i = 0; i < res.length; i++) {
             itemTable.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price]);
         }
         console.log(itemTable.toString());
-    })
-    connection.end();
+        chooseItem();
+    });
+   // connection.end();
 };
+
+function chooseItem(){
+    connection.query('SELECT item_id FROM products', function (err, res){
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: 'itemID',
+                type: 'input',
+                message: 'Please type the ID for the item you\'d like to purchase.'
+            },
+            {
+                name: 'qty',
+                type: 'input',
+                message: 'How many would you like?'
+            }
+        ]).then(function(answer){
+            connection.query('SELECT * FROM products WHERE ?',
+            { item_id: answer.itemID },function(err, res) {
+                if (err) throw err;
+
+                if (answer.qty > res[0].stock_quantity){
+                    console.log('Insufficient quantity');
+                    connection.end();
+                } else {
+                    console.log('In stock!');
+                    connection.end();
+                };
+            });
+        });
+    });
+};
+
+// show customer total cost of their purchase
